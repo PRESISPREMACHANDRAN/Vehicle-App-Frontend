f4e745f5744b71aba18ffe38814bf41529f1aa32
PRESISPREMACHANDRAN <presipremachandran@gmail.com>
2024-01-06 19:42:20 +0530

    value read from form

--- src/Components/DeleteVehicle.jsx
+++ src/Components/DeleteVehicle.jsx
@@ -1,29 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './Header'
 
 const DeleteVehicle = () => {
+    const [input, changeInput] = useState({
+
+      modelName: ""
+
+    });
+
+    const readValue = (e) => {
+      changeInput({ ...input, [e.target.name]: e.target.value });
+    };
+
+    const submitValue = () => {
+      console.log(input);
+    };
   return (
     <div>
-      <Header/>
+      <Header />
       <div className="container">
         <div className="row">
           <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-
             <div className="row g-3">
               <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-
-                <label htmlFor="" className="form-label">Model Name</label>
-                <input type="text" className="form-control" />
+                <label htmlFor="" className="form-label">
+                  Model Name
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="modelName"
+                  value={input.modelName}
+                  onChange={readValue}
+                />
               </div>
               <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                <button className="btn btn-success">Delete</button>
+                <button className="btn btn-success" onClick={submitValue}>Delete</button>
               </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default DeleteVehicle
--- src/Components/SearchVehicle.jsx
+++ src/Components/SearchVehicle.jsx
@@ -1,27 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './Header'
 
 const SearchVehicle = () => {
+      const [input, changeInput] = useState({
+        modelName: "",
+      });
+
+      const readValue = (e) => {
+        changeInput({ ...input, [e.target.name]: e.target.value });
+      };
+
+      const submitValue = () => {
+        console.log(input);
+      };
   return (
     <div>
-      <Header/>
+      <Header />
       <div className="container">
         <div className="row">
           <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
             <div className="row g-3">
               <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                <label htmlFor="" className="form-label">Model Name</label>
-                <input type="text" className="form-control" />
+                <label htmlFor="" className="form-label">
+                  Model Name
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="modelName"
+                  value={input.modelName}
+                  onChange={readValue}
+                />
               </div>
               <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                <button className="btn btn-success">Search</button>
+                <button className="btn btn-success" onClick={submitValue}>Search</button>
               </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default SearchVehicle
